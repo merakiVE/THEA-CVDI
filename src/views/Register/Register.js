2a4f1c08c5f7d818d93b012e9ee6cdba0de18c88
Jesus Navas <jesusnavas2210@gmail.com>
2017-09-10 04:05:13 -0400

    [ADD] Se agrego la vista Register

---
+++ src/views/Register/Register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+  render() {
+    return (
+      <div className="App">
+      	PAGINA DE REGISTRO
+      </div>
+    );
+  }
+}
+
+
+export default Register;
