6386ba385009ffa2d9a080da7866be7115557d3b
danzt <danztty@gmail.com>
2017-09-10 05:20:11 -0400

    [MOD]Modificado archivo login levantando el front de login con css. [ADD] agregue el gitignore.

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,14 +1,66 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import './Login.css'
+import {Form, Icon, Input, Button, Checkbox} from 'antd';
+
+const FormItem = Form.Item;
 
 class Login extends Component {
-  render() {
-    return (
-      <div className="App">
-      	PAGINA DE LOGIN
-      </div>
-    );
-  }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+
+    render() {
+        const {getFieldDecorator} = this.props.form;
+        return (
+            <div className="login">
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <Form onSubmit={this.handleSubmit} className="login-form">
+                    <h3 className="login-tittle">Log In</h3>
+                    <br/>
+                    <FormItem>
+                        {getFieldDecorator('userName', {
+                            rules: [{required: true, message: 'Please input your username!'}],
+                        })(
+                            <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="Username"/>
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('password', {
+                            rules: [{required: true, message: 'Please input your Password!'}],
+                        })(
+                            <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>} type="password"
+                                   placeholder="Password"/>
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('remember', {
+                            valuePropName: 'checked',
+                            initialValue: true,
+                        })(
+                            <Checkbox>Remember me</Checkbox>
+                        )}
+                        <a className="login-form-forgot" href="">Forgot password</a>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                        </Button>
+                        Or <a href="">register now!</a>
+                    </FormItem>
+                </Form>
+            </div>
+        );
+    }
 }
 
+const WrappedNormalLoginForm = Form.create()(Login);
 
-export default Login;
+export default WrappedNormalLoginForm;
