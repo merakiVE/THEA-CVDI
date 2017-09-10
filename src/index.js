b90aa6c7e7d08b6eef5d760c257a98a4b64ccedf
Jesus Navas <jesusnavas2210@gmail.com>
2017-09-10 04:04:12 -0400

    [MOD] Se agrego la vista de inicio del usuario

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
 // Rutes
 import AppRoutes from './AppRoutes';
