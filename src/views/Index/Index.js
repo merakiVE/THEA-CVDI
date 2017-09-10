1d61c6420df849e76e3754e674f22e9f3768d768
Jesus Navas <jesusnavas2210@gmail.com>
2017-09-10 04:19:57 -0400

    [MOD] Se agregaron botones para probar las rutas

--- src/views/Index/Index.js
+++ src/views/Index/Index.js
@@ -1,13 +1,31 @@
+//Dependencies
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+// Ant Disign
+import Button from 'antd/lib/button';
 
 class Index extends Component {
-  render() {
-    return (
-      <div className="App">
-      	PAGINA PRINCIPAL
-      </div>
-    );
-  }
+  	render() {
+	    return (
+	      	<div className="">
+	      		PAGINA PRINCIPAL
+	      		<br/>
+	      		<br/>
+		      	<Link to="/home">
+		      		<Button type="primary">Home</Button>
+		      	</Link>
+
+		      	<Link to="/login">
+		      		<Button type="primary">Login</Button>
+				</Link>
+
+		      	<Link to="/register">
+		      		<Button type="primary">Register</Button>
+		      	</Link>
+	      	</div>
+	    );
+  	}
 }
 
 
