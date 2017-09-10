1d735725fc8c8bcb8057f2d9436b299192aa6811
Jesus Navas <jesusnavas2210@gmail.com>
2017-09-10 04:06:48 -0400

    [ADD] Se agrego la vista Login

---
+++ src/views/Login/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="App">
+      	PAGINA DE LOGIN
+      </div>
+    );
+  }
+}
+
+
+export default Login;
